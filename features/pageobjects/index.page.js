98a2ae8960d8f0b86cdb992444131dee7d2d289b
Tamás Krizsán <tamas@krizsanmunka.hu>
2025-06-21 20:39:36 +0200

    refector: change some selectors to css Id/class selectors

--- features/pageobjects/index.page.js
+++ features/pageobjects/index.page.js
@@ -8,11 +8,9 @@ class IndexPage extends BasePage {
     super(url)
   }
 
-
   get sortTitle(){  
     return $('.grid-title');
   }
-
 
   get dropdownMenu(){
     return $('#menu');
@@ -22,44 +20,48 @@ class IndexPage extends BasePage {
     return $('#language');
   }
 
-
   get logoutButton(){
     return $('[data-test="nav-sign-out"]');
   }
-
 
   get DeButton(){
    return $('[data-test="lang-de"]');
   }
 
-
   get searchResults(){
     return $('#app-overview');
   }
-
-
-  async checkSortTitleLanguage(text){  
-    await expect(this.sortTitle.getValue(text));
-  }
-
-
-  async checkSearchResults(text){  
-    await expect(this.searchResults.getValue(text));
-  }
-
 
   get searchBox(){
     return $('#search-query');
   }
 
-
   async setSearchBox(text){
     await this.searchBox.setValue(text);
   }
 
-
   get searchButton(){
     return $('[data-test="search-submit"]');
+  }
+
+  get accountTitle(){  
+    return $('[data-test="page-title"]');
+  }
+
+  get searchTerm(){
+    return $('[data-test="search-term"]');
+  }
+
+  get searchCaption(){
+    return $('[data-test="search-caption"]');
+  }
+
+  get cardTitle(){
+    return $('[data-test="product-name"]');
+  }
+
+  get noResultsText(){
+    return $('[data-test="no-results"]');
   }
 
 }
