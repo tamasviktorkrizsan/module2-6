36c953d85d6f37f4aa4a3a8cd5b2c4fc70453e06
Tamás Krizsán <tamas@krizsanmunka.hu>
2025-06-22 21:21:28 +0200

    refactor: move step files to src/tests/step-definitions

--- src/tests/step-definitions/loginSteps.js
+++ src/tests/step-definitions/loginSteps.js
@@ -2,8 +2,8 @@ import {browser} from '@wdio/globals';
 import { Given, When, Then } from '@wdio/cucumber-framework';
 import { expect, assert } from 'chai';
 
-import LoginPage from '../pageobjects/login.page.js';
-import IndexPage from '../pageobjects/index.page.js';
+import LoginPage from '../../po/login.page.js';
+import IndexPage from '../../po/index.page.js';
 
 let loginPage = new LoginPage(process.env.URL + process.env.LOGIN_PATH);
 
